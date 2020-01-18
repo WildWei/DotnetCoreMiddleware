b59ff64118c177efaa8a02e4086f9a814da0d41b
William <a0937529646@gmail.com>
2020-01-18 15:53:47 +0800

    push middleware example

---
+++ MiddlewareTest/MiddlewareTest/Middleware/MyFirstMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddlewareTest.Middleware
+{
+    public class MyFirstMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyFirstMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync($"{nameof(MyFirstMiddleware)} in. \r\n");
+
+            await _next(context);
+
+            await context.Response.WriteAsync($"{nameof(MyFirstMiddleware)} out. \r\n");
+        }
+    }
+}
